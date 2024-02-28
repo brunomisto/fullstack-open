d040cdfb52652fadf368f4683438b577b7b29533
misto <ottonellicopattibruno@gmail.com>
2024-02-27 21:22:29 -0300

    Add new blog form and notification

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import Login from './components/Login'
+import NewBlog from './components/NewBlog'
+import Notification from './components/Notification'
+
 import blogService from './services/blogs'
+
+import "./App.css";
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null);
+  const [notification, setNotification] = useState(null);
 
   useEffect(() => {
     const loggedUser = localStorage.getItem("loggedUser");
@@ -17,16 +23,52 @@ const App = () => {
     )
   }, [])
 
+  const logInfo = (message) => {
+    setNotification({
+      message,
+      type: "info"
+    });
+    setTimeout(() => {
+      setNotification(null);
+    }, 5000);
+  }
+
+  const logError = (message) => {
+    setNotification({
+      message,
+      type: "error"
+    });
+    setTimeout(() => {
+      setNotification(null);
+    }, 5000);
+  }
+
   const handleLogout = () => {
     localStorage.removeItem("loggedUser");
     setUser(null);
+  }
+  
+  const createBlog = async (blog) => {
+    try {
+      const createdBlog = await blogService.create(blog, user.token);
+      setBlogs(blogs.concat(createdBlog));
+      logInfo(`a new blog ${createdBlog.title} by ${createdBlog.author} added`);
+    } catch (error) {
+      logError(error.response.data.error);
+    }
   }
 
   if (user) {
     return (
       <div>
+        {
+          notification
+          ? <Notification notification={notification} />
+          : "" 
+        }
         <h2>blogs</h2>
         <p>{user.name} logged in</p> <button onClick={handleLogout}>logout</button>
+        <NewBlog createBlog={createBlog} />
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
---
+++ part5/bloglist-frontend/src/components/NewBlog.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const NewBlog = ({ createBlog }) => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [url, setUrl] = useState("");
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={async (event) => {
+        event.preventDefault();
+        await createBlog({
+          title,
+          author,
+          url
+        });
+        setTitle("");
+        setAuthor("");
+        setUrl("");
+      }}>
+        <div>
+          <label>
+            title:
+            <input value={title} onChange={({target}) => setTitle(target.value)} />
+          </label>
+        </div>
+        <div>
+          <label>
+            author:
+            <input value={author} onChange={({target}) => setAuthor(target.value)} />
+          </label>
+        </div>
+        <div>
+          <label>
+            url:
+            <input value={url} onChange={({target}) => setUrl(target.value)} />
+          </label>
+        </div>
+        <button type="submit">add</button>
+      </form>
+    </div>
+  )
+};
+
+export default NewBlog;
