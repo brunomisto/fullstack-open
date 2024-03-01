788be91e7d1bafdab2a9491fe90dba186891c0e2
misto <ottonellicopattibruno@gmail.com>
2024-03-01 17:18:43 -0300

    Add jest global configuration to eslint

--- part5/bloglist-frontend/src/components/Like.test.jsx
+++ part5/bloglist-frontend/src/components/Like.test.jsx
@@ -1,6 +1,3 @@
-/* eslint-disable no-undef */
-/* eslint-disable import/no-extraneous-dependencies */
-
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
