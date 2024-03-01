2c8f5c84cd35f9489574cbf874709e8b8f5b8081
misto <ottonellicopattibruno@gmail.com>
2024-03-01 17:25:20 -0300

    Remove backend error handling, add cypress and ignore completed parts

--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -2,20 +2,7 @@ const jwt = require('jsonwebtoken');
 
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (error, request, response, next) => {
-  switch (error.name) {
-    case 'ValidationError':
-      return response
-        .status(400)
-        .json({ error: 'usernames must be at least 3 characters long' });
-
-    case 'MongoServerError':
-      return response
-        .status(400)
-        .json({ error: 'usernames must be unique' });
-
-    default:
-      return response.status(400).json({ error: error.message });
-  }
+  response.status(400).json({ error: error.message });
 };
 
 const tokenExtractor = (request, response, next) => {
