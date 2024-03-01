8502368120b3e929d80c82300e123fe382729940
misto <ottonellicopattibruno@gmail.com>
2024-03-01 17:34:19 -0300

    Add testing route

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -5,6 +6,7 @@ const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
+const testingRouter = require('./controllers/testing');
 
 const logger = require('./utils/logger');
 const config = require('./utils/config');
@@ -27,6 +29,9 @@ app.use(middleware.tokenExtractor);
 app.use('/api/blogs', middleware.userExtractor, blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
+if (process.env.NODE_ENV === 'test') {
+  app.use('/api/testing', testingRouter);
+}
 
 app.use(middleware.errorHandler);
 
