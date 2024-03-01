20ce697b7823b03da50e369ade19eda038919e77
misto <ottonellicopattibruno@gmail.com>
2024-03-01 17:41:44 -0300

    Test login form is displayed

--- part5/bloglist-frontend/cypress/e2e/bloglist.cy.js
+++ part5/bloglist-frontend/cypress/e2e/bloglist.cy.js
@@ -1,10 +1,13 @@
 describe('Blog app', function() {
   beforeEach(function() {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
     cy.visit('http://localhost:5173')
   })
 
   it('Login form is shown', function() {
-    // ...
+    cy.get('h2').should('contain', 'Login');
+    cy.contains('username');
+    cy.contains('password');
+    cy.get('button').should('contain', 'Login');
   })
 })
