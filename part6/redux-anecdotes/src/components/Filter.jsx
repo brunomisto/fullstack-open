bac27199b10404c9e069e124dc539c7c3ee7a710
misto <ottonellicopattibruno@gmail.com>
2024-03-04 16:48:35 -0300

    Use redux toolkit in filter reducer

--- part6/redux-anecdotes/src/components/Filter.jsx
+++ part6/redux-anecdotes/src/components/Filter.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-import { changeFilter } from "../reducers/filterReducer";
+import { changeFilter } from "../reducers/store";
 
 const Filter = () => {
   const dispatch = useDispatch();
